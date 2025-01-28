c2275ad33d15dc25b047747436c78b1d868be89c
NickFalletta <nickyny29@optonline.net>
2025-01-28 09:32:37 -0500

    slotmachine

---
+++ slotmachine/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const lever = new Audio("./sounds/lever.mp3");
+lever.volume = 0.5;
+const lose = new Audio("./sounds/lose.mp3");
+lose.volume = 0.5;
+const win = new Audio("./sounds/win.mp3");
+win.volume = 0.5;
+const spin = new Audio("./sounds/spin.mp3");
+spin.volume = 0.5;
+const synth = new Audio("./sounds/synth.mp3");
+synth.volume = 0.5;
+
+function App() {
+
+  
+  
+
+  return (
+    <> 
+
+    </>
+  )
+}
+
+export default App
